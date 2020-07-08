2f7cada627cee34e1dba21718d08b466824dd1cd
victor00 <victormorelli6@gmail.com>
2020-07-08 10:15:47 -0300

    Simple Array Sum with Coding

---
+++ SimpleArraySum.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+def simpleArraySum(ar):
+	sumar = 0
+	for n in ar:
+		sumar += n
+	return sumar
+
+//fptr = open("path", 'w')
+//ar_count = int(input())
+//ar = list(map(int, input().rstrip().split()))
+//result = simpleArraySum(ar)
+//fptr.write(str(result) + '\n')
+//fptr.close()
